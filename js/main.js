1d566c71d6dccc6c331b03c315e2d91a6bdca3ee
Phuong Tran <chiphuong9299@hotmail.com>
2023-05-28 01:57:21 +0700

    commit

--- js/main.js
+++ js/main.js
@@ -12,8 +12,6 @@ $(function () {
         },
     });
 
-    $("#league").select2();
-
     $(".shortcut-buttons-flatpickr-button").addClass("text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800");
 
     $("#btn-search").on("click", function () {
