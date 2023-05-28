f0b5db60916de73af01a13916dbfc44209f61913
Phuong Tran <chiphuong9299@hotmail.com>
2023-05-28 22:46:03 +0700

    commit

--- js/main.js
+++ js/main.js
@@ -154,7 +154,7 @@ function getData(date = null) {
                         async: false,
                         url: subUrl,
                         success: function (resp) {
-                            let lstQuality = ["Nhà Đài", "Backup 1", "Backup 2"];
+                            let lstQuality = ["nhà đài", "backup 1", "backup 2"];
                             $.each(resp.data.play_urls, function (si, se) {
                                 commentators = resp.data.commentators || [];
                                 commentators = commentators.map((x) => x.name);
@@ -162,7 +162,7 @@ function getData(date = null) {
                                 text += `${link} [${se.name}]` + "\n";
                                 text += hls + "\n";
                                 hlsUrls.push({ url: se.url, quality: se.name });
-                                if (!lstQuality.includes(se.name)) {
+                                if (!lstQuality.includes(se.name.toLowerCase())) {
                                     htmlTemp += `<a href="${se.url}" target="_blank" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">${se.name}</a>`;
                                 }
                             });
